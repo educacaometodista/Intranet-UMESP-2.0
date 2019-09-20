d5306ca65f11aa7a29d8026c691367dd2ed81b05
Léu Almeida <leo@webid.net.br>
2019-09-20 14:20:56 -0300

    Atualização de Rotas

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -1,6 +1,6 @@
 const data = [
   {
-    id: "gogo",
+    id: "home",
     icon: "iconsminds-newspaper",
     label: "Destaques",
     to: "/app/intranet/home",
@@ -13,29 +13,104 @@ const data = [
     // ]
   },
   {
-    id: "secondmenu",
+    id: "setores",
     icon: "iconsminds-business-man",
     label: "Setores",
-    to: "/app/second-menu",
+    to: "/app/setores",
     subs: [
       {
-        icon: "simple-icon-paper-plane",
-        label: "menu.second",
-        to: "/app/second-menu/second"
+        label: "Assessoria de Relações Internacionais",
+        to: "/app/setores/assessoria-de-relacoes-internacionais"
+      },
+      {
+        label: "Assessoria para Inclusão",
+        to: "/app/setores/assessoria-para-inclusao"
+      },
+      {
+        label: "Assessoria de Espaço Físico",
+        to: "/app/setores/assessoria-de-espaco-fisico"
+      },
+      {
+        label: "Centro de Convivência",
+        to: "/app/setores/centro-de-convivencia"
+      },
+      {
+        label: "Comunicação e Marketing",
+        to: "/app/setores/comunicacao-e-marketing"
+      },
+      {
+        label: "Conselho Universitário",
+        to: "/app/setores/conselho-universitario"
+      },
+      {
+        label: "Correspondência",
+        to: "/app/setores/correspondencia"
+      },
+      {
+        label: "Espaço Metô",
+        to: "/app/setores/espaco-meto"
+      },
+      {
+        label: "Gedoc",
+        to: "/app/setores/gedoc"
+      },
+      {
+        label: "Gestão de Pessoas",
+        to: "/app/setores/gestao-de-pessoas"
+      },
+      {
+        label: "Higiene Ambiental",
+        to: "/app/setores/higiene-ambiental"
+      },
+      {
+        label: "Jardinagem",
+        to: "/app/setores/jardinagem"
+      },
+      {
+        label: "Manutenção",
+        to: "/app/setores/manutencao"
+      },
+      {
+        label: "Multimeios",
+        to: "/app/setores/multimeios"
+      },
+      {
+        label: "Núcleo de Arte e Cultura",
+        to: "/app/setores/nac"
+      },
+      {
+        label: "Núcleo de Formação Cidadã",
+        to: "/app/setores/nfc"
+      },
+      {
+        label: "Pastoral Universitária e Escolar",
+        to: "/app/setores/pastoral"
+      },
+      {
+        label: "Segurança Patrimonial",
+        to: "/app/setores/seguranca-patrimonial"
+      },
+      {
+        label: "Secretaria Geral",
+        to: "/app/setores/secretaria-geral"
+      },
+      {
+        label: "Transporte",
+        to: "/app/setores/transporte"
       }
     ]
   },
   {
-    id: "blankpage",
+    id: "servicos",
     icon: "iconsminds-suitcase",
     label: "Serviços",
     to: "/app/blank-page"
   },
   {
-    id: "blankpage",
+    id: "aniversariantes",
     icon: "iconsminds-calendar-4",
     label: "Aniversariantes",
-    to: "/"
+    to: "/app/aniversariantes"
   }
 ];
 export default data;
